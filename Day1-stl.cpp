eb822ccefce2ed7263d861d17d2fe58acf55d0e9
Manishbachhane <manishbachhane@gmail.com>
2025-09-03 22:25:12 +0530

    STL basics

---
+++ Day1-stl.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // Fast IO
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << "STL Practice Started 🚀" << endl;
+
+  // Example: vector
+  vector<int> v = {1, 2, 3, 4, 5};
+  cout << "Vector elements: ";
+  for (int x : v)
+    cout << x << " ";
+  cout << endl;
+
+  return 0;
+}
